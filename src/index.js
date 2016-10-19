05373eda884923e3ba027c0713f5b70295300632
BJR Matos <bjrmatos@gmail.com>
2016-10-19 13:53:19 -0500

    [add] nullable type in exports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
 import mixed from './mixed';
+import nullable from './nullable';
 import string from './string';
 import boolean from './boolean';
 import number from './number';
 import object from './object';
 import array from './array';
 
-// TODO: implement $ref
-
 export default {
   any: mixed,
   mixed,
+  nullable,
   string,
   boolean,
   number,
