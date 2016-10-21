b63cdf4065671b588ff497796aa1712a1fcf9a68
BJR Matos <bjrmatos@gmail.com>
2016-10-21 17:07:53 -0500

    [update] tests to use `json-stable-stringify` to get deterministic json output

--- test/utils.js
+++ test/utils.js
@@ -1,13 +1,17 @@
 
 import chai from 'chai';
+import json from 'json-stable-stringify';
 
 const expect = chai.expect;
 
 export function getJSON(value, separator) {
-  return JSON.stringify(value, null, separator);
+  // using json-stable-stringify to get a deterministic json output
+  return json(value, {
+    space: separator
+  });
 }
 
 export function verifySchemaOutput(schema, expected) {
-  expect(schema.generate()).to.be.eql(getJSON(expected));
+  expect(json(schema.generate({ obj: true }))).to.be.eql(getJSON(expected));
   expect(schema.generate({ obj: true })).to.be.eql(expected);
 }
